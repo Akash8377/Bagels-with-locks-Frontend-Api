fb45f27e3f387bd12f695251d3a462692ad362e9
Akash8377 <akashchoudhary8377@gmail.com>
2024-09-10 08:53:37 +0530

    file added

--- controllers/top.js
+++ controllers/top.js
@@ -5,7 +5,7 @@ const conn = require("../services/db");
 exports.get = (req, res) => {
   // Updated SQL query with JOIN
   let sqlQuery = `
-    SELECT top.id, top.week_id, top.home_team, top.away_team, top.away_image, top.home_image, top.winning_team, top.status, top.created_at, top.updated_at, week.week_name
+    SELECT top.id, top.week_id, top.home_team, top.away_team, top.away_image, top.home_image, top.winning_team,winning_value, top.status, top.created_at, top.updated_at, week.week_name
     FROM top
     LEFT JOIN week ON top.week_id = week.id
   `;

d990637f28958f11891d02688bbcbd554c331570
bipinlong <182752867+bipinlong@users.noreply.github.com>
2024-11-01 14:52:12 +0530

    fixed forgot password

--- helper/sendMail.js
+++ helper/sendMail.js
@@ -5,11 +5,15 @@ const sendMail = async (email, mailSubjet, content) => {
   try {
     const transport = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      // port: 587,
+      // secure: false,
+      port: 465,
+      secure: true,
       auth: {
-        user: "vinod.mediasearchgroup@gmail.com",
-        pass: "ieee leja pfsc ptix",
+        // user: "vinod.mediasearchgroup@gmail.com",
+        // pass: "ieee leja pfsc ptix",
+        user: process.env.SMTP_MAIL,  // Gmail address from .env
+        pass: process.env.SMTP_PASSWORD, 
       },
     });
     const mailOption = {
